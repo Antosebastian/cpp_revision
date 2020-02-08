a417ca5944dbafc146cf7ab0e21cfac903d88aaf
Anto Sebastian <51168158+Antosebastian@users.noreply.github.com>
2020-02-07 23:44:45 -0500

    Add files via upload

---
+++ sec 9/countdown.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+{
+	int num {};
+	cout<<"Enter a integer - start countdown \n";
+	cin>>num;
+	while(num>0)
+	{
+		cout<<num<<endl;
+		--num;
+	}
+	cout<<endl;
+return 0;
+}
